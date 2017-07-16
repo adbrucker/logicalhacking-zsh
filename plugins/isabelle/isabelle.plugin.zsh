bc7985c58027c7e304a8caa9aae8b2076c15fc38
Achim D. Brucker <adbrucker@0x5f.org>
2017-07-16 11:39:59 +0100

    Isabelle module. Initial version providing function to strip Isabelle/ML/Pascal-style comments.

---
+++ plugins/isabelle/isabelle.plugin.zsh
@@ -0,0 +1,51 @@
+# vim:ft=zsh ts=4 sw=2 sts=2
+#
+# Isabelle Plugin for ZSH
+#
+# Copyright (C) 2017 Achim D. Brucker, https://www.brucker.ch
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+zmodload zsh/pcre
+
+isabelle-strip-comments() {
+    local stripped
+    stripped=` echo "$1" | awk -e '
+        BEGIN {
+            found=0;
+        }{
+            spos=index($0,"(*");
+            epos=index($0,"*)");
+            if(spos > 0 && epos ==0) {
+                printf("%s\n",substr($0,1,spos-1));
+                found=1;
+            } else if(spos == 0 && epos >0) {
+                found=0;
+                if(length($0) != epos+1) {
+                    printf("%s\n",substr($0,epos+2));
+                }
+            } else if(spos > 0 && epos > 0) {
+                printf("%s %s\n",substr($0,1,spos-1),substr($0,epos+2));
+            } else if(found==0) {
+                print;
+            }
+        }' `
+     echo "$stripped"   
+}
